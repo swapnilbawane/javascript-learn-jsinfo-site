c3c5b86e60611ee16b90d2990d3767e8eb213d9f
swapnilbawane <swapnilbawane08@gmail.com>
2022-02-25 18:18:21 +0530

    initial commit

---
+++ examples.js
@@ -0,0 +1,28 @@
+// alert("I am just separate alert!")
+
+// let message="I am Javascript!";
+// alert(message);
+
+// let user="Tim", age=65, job="CEO";
+// alert("Hello"+user+","+"You entered age as"+age+","+"Your job designation is"+job);
+
+// let admin;
+// let name;
+// name="John";
+// admin=name;
+// alert(admin);
+
+// const ourPlanetName="EARTH";
+// let currentuserName="John";
+
+// let name="Helassr";
+// alert(`hello ${1}`);
+// alert(`hello ${"name"}`);
+// alert(`hello ${name}`);
+
+// let isBoss = confirm("Are you the boss?");
+// alert(isBoss);
+
+let userName = prompt("What is your name");
+alert(`Hello ${userName}`);
+
